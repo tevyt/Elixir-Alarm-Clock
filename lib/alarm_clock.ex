2e5d9784ebd487ea44e6f8a03ce8b387837a85af
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-08-20 18:20:05 -0500

    Handles pm period

--- lib/alarm_clock.ex
+++ lib/alarm_clock.ex
@@ -12,11 +12,25 @@ defmodule AlarmClock do
     end
   end
 
+  def date_time_from_args(time_args) do
+    current_date_time = DateTime.utc_now
+    case time_args do
+      {hour, minute, "am"} -> current_date_time
+      |> Map.update!(:hour, fn(_) -> hour + 5 end) #Convert to my timezone, not much built-in Timezone handling in Elixir yet
+      |> Map.update!(:minute, fn(_) -> minute end)
+      {hour, minute, "pm"} -> current_date_time
+      |> Map.update!(:hour, fn(_)  -> hour + 17 end)
+      |> Map.update!(:minute, fn(_) -> minute end)
+    end
+  end
+
   @doc """
   Main function entry point of the program
   """
   def main(args) do
     args
     |> parse_args
+    |> date_time_from_args
+    |> IO.inspect
   end
 end
