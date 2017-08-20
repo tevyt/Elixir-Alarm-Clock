bc3e6254b936b61fb9b37b615a00bcb0eb0b6c60
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-08-20 14:40:45 -0500

    Parses hour argument

--- lib/alarm_clock.ex
+++ lib/alarm_clock.ex
@@ -2,10 +2,19 @@ defmodule AlarmClock do
   @moduledoc """
   Main module for the alarm clock project all logic for the application lives here.
   """
+  def parse_args(args) do
+    case OptionParser.parse(args, switches: [hour: :integer]) do
+      {[hour: hour], _, _} -> hour
+      _ -> {:error, "Invalid usage"}
+    end
+  end
 
   @doc """
+  Main function entry point of the program
   """
   def main(args) do
-    IO.puts args
+    args
+    |> parse_args
+    |> IO.inspect
   end
 end
