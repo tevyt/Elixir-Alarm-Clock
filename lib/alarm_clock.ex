1765860f8471406cc5ba3c7469444cd59bfd27f1
Travis Smith <travis.alexander.smith.ja@gmail.com>
2017-08-20 15:35:31 -0500

    Handles period option

--- lib/alarm_clock.ex
+++ lib/alarm_clock.ex
@@ -3,11 +3,12 @@ defmodule AlarmClock do
   Main module for the alarm clock project all logic for the application lives here.
   """
   def parse_args(args) do
-    parsed = OptionParser.parse(args, switches: [hour: :integer, minute: :integer]) 
+    parsed = OptionParser.parse(args, switches: [hour: :integer, minute: :integer, period: :string]) 
     IO.inspect parsed
     case parsed do
-      {[hour: hour], _, _} -> {hour, 0}
-      {[hour: hour, minute: minute], _, _} -> {hour, minute}
+      {[hour: hour], _, _} -> {hour, 0, "am"}
+      {[hour: hour, minute: minute], _, _} -> {hour, minute, "am"}
+      {[hour: hour, minute: minute, period: period], _, _} -> {hour, minute, period}
       _ -> {:error, "Invalid usage"}
     end
   end
